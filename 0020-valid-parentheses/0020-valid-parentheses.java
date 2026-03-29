8ad7b56aa52a1d3636cd9733559edc2d817140ac
Malireddy Meghana <malireddymeghana387@gmail.com>
2026-03-29 18:51:40 +0530

    Time: 4 ms (41.27%), Space: 43.4 MB (34.59%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,23 +1,42 @@
 class Solution {
     public boolean isValid(String s) {
+        // Stack<Character> stk=new Stack<>();
+        // for(int i=0;i<s.length();i++){
+        //     if(s.charAt(i)=='('||s.charAt(i)=='{'||s.charAt(i)=='['){
+        //         stk.push(s.charAt(i));
+
+        //     else{
+        //         if(stk.isEmpty()) return false;
+        //         if(s.charAt(i)==']'&&stk.peek()=='['){
+        //             stk.pop();
+        //         }
+        //         else if(s.charAt(i)=='}'&&stk.peek()=='{'){
+        //             stk.pop();
+        //         }
+        //         else if(s.charAt(i)==')'&&stk.peek()=='('){
+        //             stk.pop();
+        //         }
+        //         else return false;
+        //         }
+        // }
+        // return stk.isEmpty();
         Stack<Character> stk=new Stack<>();
+        HashMap<Character,Character> hmap=new HashMap<>();
+        hmap.put(')','(');
+        hmap.put('}','{');
+        hmap.put(']','[');
         for(int i=0;i<s.length();i++){
-            if(s.charAt(i)=='('||s.charAt(i)=='{'||s.charAt(i)=='['){
+            if(s.charAt(i)=='('||s.charAt(i)=='['||s.charAt(i)=='{'){
                 stk.push(s.charAt(i));
             }
             else{
                 if(stk.isEmpty()) return false;
-                if(s.charAt(i)==']'&&stk.peek()=='['){
+                if(stk.peek()==hmap.get((s.charAt(i)))){
                     stk.pop();
                 }
-                else if(s.charAt(i)=='}'&&stk.peek()=='{'){
-                    stk.pop();
-                }
-                else if(s.charAt(i)==')'&&stk.peek()=='('){
-                    stk.pop();
-                }
-                else return false;
-                }
+                else
+                    return false;
+            }
         }
         return stk.isEmpty();
 }
