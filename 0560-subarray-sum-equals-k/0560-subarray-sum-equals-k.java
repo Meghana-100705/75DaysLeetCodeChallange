99cc17aa4e0144c0198d119bea50e1b8cebd7bb1
Malireddy Meghana <malireddymeghana387@gmail.com>
2026-05-12 13:13:34 +0530

    Time: 1548 ms (10.55%), Space: 48.4 MB (87.46%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,10 +1,21 @@
 class Solution {
     public int subarraySum(int[] nums, int tar) {
+        // int count=0;
+        // for(int i=0;i<nums.length;i++){
+        //     int sum=0;
+        //     for(int j=i;j<nums.length;j++){
+        //             sum+=nums[j];
+        //         if(sum==tar){
+        //             count++;
+        //         }
+        //     }
+        // }
+        // return count;
         int count=0;
         for(int i=0;i<nums.length;i++){
             int sum=0;
             for(int j=i;j<nums.length;j++){
-                    sum+=nums[j];
+                sum+=nums[j];
                 if(sum==tar){
                     count++;
                 }
