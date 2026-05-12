e61142c77bcc7757c423de671320ff7082aabccd
Malireddy Meghana <malireddymeghana387@gmail.com>
2026-05-12 19:05:57 +0530

    Time: 49 ms (8.46%), Space: 47.2 MB (40.41%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,24 +1,34 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        // int[] ans=new int[2];
+        // // int[] ans=new int[2];
+        // // for(int i=0;i<nums.length;i++){
+        // //     for(int j=i+1;j<nums.length;j++){
+        // //         if(nums[i]+nums[j]==target){
+        // //             ans[0]=i;
+        // //             ans[1]=j;
+        // //             break;
+        // //         }
+        // //     }
+        // // }
+        // // return ans;
+        // HashMap<Integer,Integer> hmap=new HashMap<>();
         // for(int i=0;i<nums.length;i++){
-        //     for(int j=i+1;j<nums.length;j++){
-        //         if(nums[i]+nums[j]==target){
-        //             ans[0]=i;
-        //             ans[1]=j;
-        //             break;
-        //         }
+        //     int c=target-nums[i];
+        //     if(hmap.containsKey(c)){
+        //         return new int[]{hmap.get(c),i};
         //     }
+        //         hmap.put(nums[i],i);
         // }
-        // return ans;
-        HashMap<Integer,Integer> hmap=new HashMap<>();
+        // return new int[0];
+        int ans[]=new int[2];
         for(int i=0;i<nums.length;i++){
-            int c=target-nums[i];
-            if(hmap.containsKey(c)){
-                return new int[]{hmap.get(c),i};
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[i]+nums[j]==target){
+                    ans[0]=i;
+                    ans[1]=j;
+                }
             }
-                hmap.put(nums[i],i);
         }
-        return new int[0];
+        return ans;
     }
 }
