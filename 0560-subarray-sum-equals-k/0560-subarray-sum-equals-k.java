a36a54d1291fbdbaaa13edeee6782b0173b972a6
Malireddy Meghana <malireddymeghana387@gmail.com>
2026-05-17 19:31:36 +0530

    Time: 1536 ms (16.9%), Space: 48.2 MB (96.29%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -11,6 +11,17 @@ class Solution {
         //     }
         // }
         // return count;
+        // int count=0;
+        // for(int i=0;i<nums.length;i++){
+        //     int sum=0;
+        //     for(int j=i;j<nums.length;j++){
+        //         sum+=nums[j];
+        //         if(sum==tar){
+        //             count++;
+        //         }
+        //     }
+        // }
+        // return count;
         int count=0;
         for(int i=0;i<nums.length;i++){
             int sum=0;
