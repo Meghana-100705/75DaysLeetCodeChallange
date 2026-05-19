1c5a216533dc2a9dad9db5e057b039ced3bccca5
Malireddy Meghana <malireddymeghana387@gmail.com>
2026-05-19 20:15:37 +0530

    Time: 31 ms (65.47%), Space: 95.6 MB (43.72%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -1,41 +1,58 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
-    //     int longest=0;
-    //     for(int i=0;i<nums.length;i++){
-    //         int x=nums[i];
-    //         int count=1;
-    //         while(search(nums,x+1)){
-    //             x=x+1;
-    //             count+=1;
-    //         }
-    //         longest=Math.max(count,longest);
-    //     }
-    //     return longest;
+    // //     int longest=0;
+    // //     for(int i=0;i<nums.length;i++){
+    // //         int x=nums[i];
+    // //         int count=1;
+    // //         while(search(nums,x+1)){
+    // //             x=x+1;
+    // //             count+=1;
+    // //         }
+    // //         longest=Math.max(count,longest);
+    // //     }
+    // //     return longest;
+    // // }
+    // //     static boolean search(int nums[],int target){
+    // //         for(int num:nums){
+    // //             if(num==target){
+    // //                 return true;
+    // //             }
+    // //         }
+    // //         return false;
+    // // Arrays.sort(nums);
+    // // int count=1;
+    // // int longest=1;
+    // // if(nums.length==0) return 0;
+    // // for(int i=0;i<nums.length-1;i++){
+    // //     if(nums[i]+1==nums[i+1]){
+    // //         count++;
+    // //     }
+    // //     else if(nums[i]==nums[i+1]){
+    // //         continue;
+    // //     }
+    // //     else{
+    // //         longest=Math.max(longest,count);
+    // //         count=1;
+    // //     }
+    // // }
+    // // longest=Math.max(longest,count);
+    // // return longest;
+    // HashSet<Integer> hset=new HashSet<>();
+    // for(int num:nums){
+    //     hset.add(num);
     // }
-    //     static boolean search(int nums[],int target){
-    //         for(int num:nums){
-    //             if(num==target){
-    //                 return true;
-    //             }
-    //         }
-    //         return false;
-    // Arrays.sort(nums);
-    // int count=1;
-    // int longest=1;
-    // if(nums.length==0) return 0;
-    // for(int i=0;i<nums.length-1;i++){
-    //     if(nums[i]+1==nums[i+1]){
+    // int longest=0;
+    // for(int num:hset){
+    // if(!hset.contains(num-1)){
+    //     int current=num;
+    //     int count=1;
+    //     while(hset.contains(current+1)){
+    //         current++;
     //         count++;
     //     }
-    //     else if(nums[i]==nums[i+1]){
-    //         continue;
-    //     }
-    //     else{
-    //         longest=Math.max(longest,count);
-    //         count=1;
-    //     }
+    //     longest=Math.max(count,longest);
     // }
-    // longest=Math.max(longest,count);
+    // }
     // return longest;
     HashSet<Integer> hset=new HashSet<>();
     for(int num:nums){
@@ -44,10 +61,10 @@ class Solution {
     int longest=0;
     for(int num:hset){
     if(!hset.contains(num-1)){
-        int current=num;
+        int curr=num;
         int count=1;
-        while(hset.contains(current+1)){
-            current++;
+        while(hset.contains(curr+1)){
+            curr++;
             count++;
         }
         longest=Math.max(count,longest);
