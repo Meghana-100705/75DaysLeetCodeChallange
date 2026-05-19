4009890bff2ebfe0b55ec6ee161b7fc8243c0305
Malireddy Meghana <malireddymeghana387@gmail.com>
2026-05-19 11:14:37 +0530

    Time: 2 ms (91.75%), Space: 47.4 MB (92.44%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,5 +1,21 @@
 class Solution {
     public void moveZeroes(int[] nums) {
+        // int j=0;
+        // for(int i=0;i<nums.length;i++){
+        //     if(nums[i]!=0){
+        //         nums[j]=nums[i];
+        //         j++;
+        //     }
+        // }
+        // while(j<nums.length){
+        //     nums[j]=0;
+        //     j++;
+        // }
+        // for(int i=0;i<arr.length-1;i++){
+        //     if(arr[i]==0){
+        //     }
+        // }
+        // }
         int j=0;
         for(int i=0;i<nums.length;i++){
             if(nums[i]!=0){
@@ -11,6 +27,6 @@ class Solution {
             nums[j]=0;
             j++;
         }
-
     }
+
 }
