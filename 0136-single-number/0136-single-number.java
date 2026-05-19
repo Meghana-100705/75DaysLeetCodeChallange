dd3b4599acaffb6507d0436933cd3ad8cff836f9
Malireddy Meghana <malireddymeghana387@gmail.com>
2026-05-19 11:32:27 +0530

    Time: 1 ms (99.96%), Space: 46.9 MB (45.28%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -11,10 +11,10 @@ class Solution {
         //     res^=nums[i];
         //   }
         //   return res;
+        // 
         int res=0;
         for(int i=0;i<nums.length;i++){
             res^=nums[i];
-
         }
         return res;
     }
