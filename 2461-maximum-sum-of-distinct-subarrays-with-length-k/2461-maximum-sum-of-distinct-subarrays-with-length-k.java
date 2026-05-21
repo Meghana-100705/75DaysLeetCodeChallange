bf78bf205a7e04735c7bf04acb7be6cbfd04f5ed
Malireddy Meghana <malireddymeghana387@gmail.com>
2026-05-21 13:04:22 +0530

    Time: 37 ms (60.57%), Space: 98.9 MB (51.53%) - LeetHub

--- 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.java
+++ 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.java
@@ -3,21 +3,38 @@ class Solution {
         long sum=0;
         long max=0;
         int left=0;
-        HashMap<Integer,Integer> hmap=new HashMap<>();
+        // HashMap<Integer,Integer> hmap=new HashMap<>();
+        // for(int right=0;right<nums.length;right++){
+        //     sum+=nums[right];
+        //     hmap.put(nums[right],hmap.getOrDefault(nums[right],0)+1);
+        //     if(right-left+1>k){
+        //         sum-=nums[left];
+        //         hmap.put(nums[left],hmap.get(nums[left])-1);
+        //         if(hmap.get(nums[left])==0){
+        //             hmap.remove(nums[left]);
+        //         }
+        //     left++;
+        //     }
+        // if(right-left+1==k&&hmap.size()==k){
+        //     max=Math.max(max,sum);
+        // }
+        // }
+        // return max;
+        HashSet<Integer> hset=new HashSet<>();
         for(int right=0;right<nums.length;right++){
-            sum+=nums[right];
-            hmap.put(nums[right],hmap.getOrDefault(nums[right],0)+1);
-            if(right-left+1>k){
+            while(hset.contains(nums[right])){
+                hset.remove(nums[left]);
                 sum-=nums[left];
-                hmap.put(nums[left],hmap.get(nums[left])-1);
-                if(hmap.get(nums[left])==0){
-                    hmap.remove(nums[left]);
-                }
-            left++;
+                left++;
             }
-        if(right-left+1==k&&hmap.size()==k){
-            max=Math.max(max,sum);
-        }
+            hset.add(nums[right]);
+            sum+=nums[right];
+            if(right-left+1==k){
+                max=Math.max(max,sum);
+                hset.remove(nums[left]);
+                sum-=nums[left];
+                left++;
+            }
         }
         return max;
     }
