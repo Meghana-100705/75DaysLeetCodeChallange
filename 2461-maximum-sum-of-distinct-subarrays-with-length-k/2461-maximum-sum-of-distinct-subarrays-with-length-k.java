6793434d84f78f037e76bef1424cb13abfb4665f
Malireddy Meghana <malireddymeghana387@gmail.com>
2026-05-25 12:37:18 +0530

    Time: 33 ms (89.72%), Space: 98.9 MB (46.38%) - LeetHub

--- 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.java
+++ 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.java
@@ -1,26 +1,9 @@
 class Solution {
     public long maximumSubarraySum(int[] nums, int k) {
-        long sum=0;
-        long max=0;
-        int left=0;
-        // HashMap<Integer,Integer> hmap=new HashMap<>();
-        // for(int right=0;right<nums.length;right++){
-        //     sum+=nums[right];
-        //     hmap.put(nums[right],hmap.getOrDefault(nums[right],0)+1);
-        //     if(right-left+1>k){
-        //         sum-=nums[left];
-        //         hmap.put(nums[left],hmap.get(nums[left])-1);
-        //         if(hmap.get(nums[left])==0){
-        //             hmap.remove(nums[left]);
-        //         }
-        //     left++;
-        //     }
-        // if(right-left+1==k&&hmap.size()==k){
-        //     max=Math.max(max,sum);
-        // }
-        // }
-        // return max;
         HashSet<Integer> hset=new HashSet<>();
+        long len=0;
+        int left=0;
+        long sum=0;
         for(int right=0;right<nums.length;right++){
             while(hset.contains(nums[right])){
                 hset.remove(nums[left]);
@@ -30,12 +13,12 @@ class Solution {
             hset.add(nums[right]);
             sum+=nums[right];
             if(right-left+1==k){
-                max=Math.max(max,sum);
+                len=Math.max(len,sum);
                 hset.remove(nums[left]);
                 sum-=nums[left];
                 left++;
             }
         }
-        return max;
+        return len;
     }
 }   
