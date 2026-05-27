eab8306418cdc12d92006d474c42ce35d92f2faf
Malireddy Meghana <malireddymeghana387@gmail.com>
2026-05-27 12:17:15 +0530

    Time: 11 ms (52.43%), Space: 46.1 MB (63.81%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,40 +1,19 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-        // int[] freq = new int[26];
-        // int left = 0;
-        // int maxFreq = 0;
-        // int maxLen = 0;
-
-        // for (int right = 0; right < s.length(); right++) {
-        //     char ch = s.charAt(right);
-        //     freq[ch - 'A']++;
-
-        //     maxFreq = Math.max(maxFreq, freq[ch - 'A']);
-
-        //     while ((right - left + 1) - maxFreq > k) {
-        //         freq[s.charAt(left) - 'A']--;
-        //         left++;
-        //     }
-
-        //     maxLen = Math.max(maxLen, right - left + 1);
-        // }
-
-        // return maxLen;
-        int[] freq=new int[26];
-        int maxfreq=0;
-        int maxlen=0;
-        int left=0;
-        for(int right=0;right<s.length();right++){
-            char ch=s.charAt(right);
-            freq[ch-'A']++;
-            maxfreq=Math.max(maxfreq,freq[ch-'A']);
-            while((right-left+1)-maxfreq>k){
-                freq[s.charAt(left)-'A']--;
-                left++;
-            }
-            maxlen=Math.max(maxlen,right-left+1);
+       int freq[]=new int[26];
+       int maxfreq=0;
+       int left=0;
+       int maxlen=0;
+       for(int right=0;right<s.length();right++){
+        freq[s.charAt(right)-'A']++;
+        maxfreq=Math.max(maxfreq,freq[s.charAt(right)-'A']);
+        int windowsize=right-left+1;
+        if(windowsize-maxfreq>k){
+            freq[s.charAt(left)-'A']--;
+            left++;
         }
-        return maxlen;
-
+        maxlen=Math.max(maxlen,right-left+1);
+       }
+       return maxlen;
     }
 }
