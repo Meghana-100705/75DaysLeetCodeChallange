9b092c7f33d7291106cd434714445cef6854ed3e
Malireddy Meghana <malireddymeghana387@gmail.com>
2026-05-28 08:38:10 +0530

    Time: 2 ms (99.9%), Space: 45.7 MB (74.24%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,22 +1,23 @@
 class Solution {
     public String minWindow(String s, String t) {
-    int freq[]=new int[123];
-    int minlen=Integer.MAX_VALUE;
+    int freq[]=new int[128];
     int left=0;
+    int min=Integer.MAX_VALUE;
     int idx=0;
     int count=0;
-    for(char c:t.toCharArray()){
-        freq[c]++;
+    String ans="";
+    if(t.length()>s.length()) return "";
+    for(int i=0;i<t.length();i++){
+        freq[t.charAt(i)]++;
     }
     for(int right=0;right<s.length();right++){
-        char ch=s.charAt(right);
-        if(freq[ch]>0){
-            count++;
+        if(freq[s.charAt(right)]>0){
+           count++;
         }
-        freq[ch]--;
+        freq[s.charAt(right)]--;
         while(count==t.length()){
-            if(right-left+1<minlen){
-                minlen=right-left+1;
+            if(right-left+1<min){
+                min=right-left+1;
                 idx=left;
             }
             char leftchar=s.charAt(left);
@@ -27,7 +28,6 @@ class Solution {
             left++;
         }
     }
-    return minlen==Integer.MAX_VALUE?"":s.substring(idx,idx+minlen);
-        
+    return min==Integer.MAX_VALUE?"":s.substring(idx,idx+min);
     }
 }
