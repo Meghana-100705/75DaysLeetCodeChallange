7925d82a17b4543dc814450b1d51658197683e2e
Malireddy Meghana <malireddymeghana387@gmail.com>
2026-05-29 16:58:44 +0530

    Time: 0 ms (100%), Space: 48.4 MB (49.09%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -1,21 +1,19 @@
 class Solution {
     public int search(int[] arr, int target) {
-    int left = 0;
-    int right = arr.length - 1;
-
-    while (left <= right) {
-       
-        int mid = left + (right - left) / 2;
-
-        if (arr[mid] == target) {
-            return mid; 
+     int low=0;
+     int high=arr.length-1;
+     while(low<=high){
+        int mid=low+(high-low)/2;
+        if(arr[mid]==target){
+            return mid;
         }
-        if (arr[mid] < target) {
-            left = mid + 1;
-        } else {
-            right = mid - 1; 
+        else if(arr[mid]<target){
+           low=mid+1;
         }
-    }
-    return -1;
+        else{
+            high=mid-1;
+        }
+     }   
+     return -1;
     }
 }
