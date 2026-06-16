77af3e1f328abdc2f7b3531256e91f233fe4d010
Malireddy Meghana <malireddymeghana387@gmail.com>
2026-06-16 21:18:09 +0530

    Time: 0 ms (100%), Space: 41.9 MB (68.02%) - LeetHub

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.java
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.java
@@ -1,5 +1,11 @@
 class Solution {
     public int getSum(int a, int b) {
-        return a+b;
+       // return a+b;
+        while (b != 0) {
+            int sum = a ^ b;
+            b = (a & b) << 1;
+            a = sum;
+        }
+        return a;
     }
 }
